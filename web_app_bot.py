cd16773ac6c55867b2f24490f5272e565798b799
dyadyaJora <idyadyajora@gmail.com>
2025-04-21 21:51:25 +0400

    init telegram web app

---
+++ web_app_bot.py
@@ -0,0 +1,54 @@
+import asyncio
+import logging
+from os import getenv
+
+import dotenv
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart
+from aiogram.types import WebAppInfo, ReplyKeyboardMarkup
+
+dotenv.load_dotenv()
+
+API_TOKEN = getenv('BOT_TOKEN')
+WEBAPP_URL = getenv('WEB_APP_URL')
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="🚀 Launch WebApp", web_app=WebAppInfo(url=WEBAPP_URL))
+        ],
+        [
+            types.KeyboardButton(text="ℹ️ Information")
+        ],
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Choose an action"
+    )
+    await message.answer("Welcome! Choose an action:", reply_markup=keyboard)
+
+
+@dp.message(F.text == "ℹ️ Information")
+async def info(message: types.Message):
+    await message.answer("This is a Telegram bot with a WebApp interface. Click the first button to launch the WebApp.")
+
+
+# @dp.message(F.content_type == types.ContentType.WEB_APP_DATA)
+@dp.message(F.web_app_data)
+async def web_app_data_handler(message: types.Message):
+    await message.answer(f"✅ Data received from WebApp:\n{message.web_app_data.data}")
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
